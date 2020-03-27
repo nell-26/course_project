2872e4d4d681011eef77dad9349e892334eaba3a
nell-26 <nell-26@users.noreply.github.com>
2020-03-27 17:59:00 +0200

    add class group and class student

---
+++ app/src/main/java/com/example/courseproject/InformationOfStudentsActivity.java
@@ -0,0 +1,14 @@
+package com.example.courseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationOfStudentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_of_students);
+    }
+}
--- app/src/main/java/com/example/courseproject/MainActivity.java
+++ app/src/main/java/com/example/courseproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.courseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         findAllView();
 
+        informationToStudens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentFromMainActivityToInformationOfStudentsActivity = new Intent(MainActivity.this, InformationOfStudentsActivity.class);
+                startActivity(intentFromMainActivityToInformationOfStudentsActivity);
+            }
+        });
     }
 
     // шукає та прив'язує кнопки
