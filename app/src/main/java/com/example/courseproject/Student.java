4ad8b3125692865cbe2ac74fb9fd3332507bd065
nell-26 <nell-26@users.noreply.github.com>
2020-03-27 18:11:56 +0200

    start realization student class

--- app/src/main/java/com/example/courseproject/Student.java
+++ app/src/main/java/com/example/courseproject/Student.java
@@ -1,4 +1,29 @@
 package com.example.courseproject;
 
 public class Student {
+    private String name;
+    private String surName;
+
+    public Student () {}
+
+    public Student (String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName () {
+        return this.surName;
+    }
+
+    public void setSurName (String surName) {
+        this.surName = surName;
+    }
 }
