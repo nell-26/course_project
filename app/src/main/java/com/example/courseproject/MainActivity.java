f5e5ae5a9320fea6649b23a7783641a8973fb03e
nell-26 <nell-26@users.noreply.github.com>
2020-03-27 17:44:10 +0200

    create first screen

--- app/src/main/java/com/example/courseproject/MainActivity.java
+++ app/src/main/java/com/example/courseproject/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.courseproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button informationToStudens;
+    private Button scheduleOfSubjects;
+    private Button iformationNb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findAllView();
+
+    }
+
+    // шукає та прив'язує кнопки
+    private void findAllView () {
+        informationToStudens = findViewById(R.id.informationToStudensButton);
+        scheduleOfSubjects = findViewById(R.id.scheduleOfSubjectsButton);
+        iformationNb = findViewById(R.id.iformationNbButton);
     }
 }
