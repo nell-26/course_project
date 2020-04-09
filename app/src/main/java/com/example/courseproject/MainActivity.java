7fcad78d3f8a0626c5d30ee2afe085ce250583fa
nell-26 <nell-26@users.noreply.github.com>
2020-04-09 23:13:00 +0300

    students

--- app/src/main/java/com/example/courseproject/MainActivity.java
+++ app/src/main/java/com/example/courseproject/MainActivity.java
@@ -59,7 +59,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createStudents () {
-        Container.addStudentInP31(new Student("Nastya", "Krupa", "26.11", "099999999", 4.6f, 0));
+        Container.addStudentInP31(new Student("Nastya", "Krupa", "26.11.2001", "0989516668", 4.6f, 0));
+        Container.addStudentInP31(new Student("Nazar", "Dub", "00/00/0000", "00000000", 3.7f, 0));
+        Container.addStudentInP31(new Student("Vitalij", "Andrejchuk", "00/00/0000", "0000000000", 3.1f, 0));
+        Container.addStudentInP31(new Student("Maksym", "Boledzuk", "00/00/0000", "0000000000", 3.f, 0));
+        Container.addStudentInP31(new Student("Andrij", "Borysevych", "00/00/0000", "0000000000", 4.6f, 0));
+        Container.addStudentInP31(new Student("Volodia", "Genyk", "00/00/0000", "0000000000", 3f, 0));
+        Container.addStudentInP31(new Student("Bogdan", "Goruk", "00/00/0000", "0000000000", 3f, 0));
+        Container.addStudentInP31(new Student("Maks", "Gzaman", "00/00/0000", "0000000000", 3.6f, 0));
+        Container.addStudentInP31(new Student("Misha", "Dudiak", "00/00/0000", "0000000000", 4f, 0));
+        Container.addStudentInP31(new Student("Oleksij", "Zaharchyk", "00/00/0000", "0000000000", 4f, 0));
+        Container.addStudentInP31(new Student("Rostic", "Kunychak", "00/00/0000", "0000000000", 2.7f, 0));
+        Container.addStudentInP31(new Student("Vasia", "Mykytiuk", "00/00/0000", "0000000000", 3f, 0));
+        Container.addStudentInP31(new Student("Vika", "Mykytiuk", "00/00/0000", "0000000000", 3f, 0));
+        Container.addStudentInP31(new Student("Dima", "Ogorodnychuk", "00/00/0000", "0000000000", 3f, 0));
+        Container.addStudentInP31(new Student("Arsen", "Palamarchuk", "00/00/0000", "0000000000", 4f, 0));
+        Container.addStudentInP31(new Student("Ruslan", "Percovyc", "00/00/0000", "0000000000", 4.6f, 0));
+        Container.addStudentInP31(new Student("Taras", "Smolin", "00/00/0000", "0000000000", 4.6f, 0));
+        Container.addStudentInP31(new Student("Arsen", "Stasiuk", "00/00/0000", "0000000000", 4.6f, 0));
+        Container.addStudentInP31(new Student("Nazar", "Stefurak", "00/00/0000", "0000000000", 4.6f, 0));
+        Container.addStudentInP31(new Student("Oleg", "Tremtiachyj", "00/00/0000", "0000000000", 4.6f, 0));
         Container.addGroupInList(new Group("P-31", "shedule", Container.getStudents_P31()));
     }
 }
