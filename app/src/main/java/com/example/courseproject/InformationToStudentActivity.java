adaba8afc90606ff0e1e74e5447d2482afd1f3af
nell-26 <nellit.krupa@gmail.com>
2020-05-24 20:37:19 +0300

    result

--- app/src/main/java/com/example/courseproject/InformationToStudentActivity.java
+++ app/src/main/java/com/example/courseproject/InformationToStudentActivity.java
@@ -2,12 +2,16 @@ package com.example.courseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InformationToStudentActivity extends AppCompatActivity {
 
     private TextView rateText;
+    private Button backButton;
     private String text = "";
 
     @Override
@@ -16,6 +20,15 @@ public class InformationToStudentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_information_to_student);
 
         rateText = findViewById(R.id.rateText);
+        backButton = findViewById(R.id.button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InformationToStudentActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         Bundle arguments = getIntent().getExtras();
--- app/src/main/java/com/example/courseproject/infoActivity.java
+++ app/src/main/java/com/example/courseproject/infoActivity.java
@@ -8,13 +8,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class infoActivity extends AppCompatActivity {
-    private Button badButtton;
+    private Button backButtton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        backButtton = findViewById(R.id.backButtonInfo);
+
+
+        backButtton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(infoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
